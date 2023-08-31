58d14b9451a30451802979fbe8d6534f1b5b014c
Ikpechukwu <110417211+circwall@users.noreply.github.com>
2023-08-31 22:09:14 +0100

    some basic changes
    changes

--- src/container/features/Features.jsx
+++ src/container/features/Features.jsx
@@ -25,7 +25,7 @@ const Features = () => {
         <h1 className='gradient__text'>
           The Future is Now You Just Need to Realize It. Step into Future Today & Make It Happen.
         </h1>
-        <p>Request early access</p>
+        <p>Request early access to get started</p>
       </div>
       <div className='gpt3__features-container'>
         {featureData.map((item, index)=>(
--- src/container/possibility/Possibility.jsx
+++ src/container/possibility/Possibility.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Possibility.css'
+import possibility from '../../assets/possibility.png'
 
 const Possibility = () => {
   return (
-    <div>
-      Possibility
+    <div className='gpt3__posibility section__padding' id ="possibility">
+      <div className='gpt3__possibility-image'>
+        <img src={possibility} alt ='possibility'/>
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request early access to get started</h4>
+        <h1  className='gradient-text'>
+          Possibilities are beyond your Imagination
+        </h1>
+        <p>As a front-end developer, I transform ideas into visually stunning and user centeric experience.</p>
+        <h4>Request early access to get started</h4>
+      </div>
     </div>
   )
 }
